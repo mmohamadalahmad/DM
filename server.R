bf9c841b264330e765b8e36ccbebc3a7e7310972
mmohamadalahmad <49354192+mmohamadalahmad@users.noreply.github.com>
2021-07-30 08:11:00 -0500

    Added files

---
+++ server.R
@@ -0,0 +1,14 @@
+diabetesrisk<-function(glucose) glucose/200
+
+shinyServer(
+  function(input, output){
+    
+    output$inputValue<-renderPrint({input$glucose})
+    output$prediction<-renderPrint({diabetesrisk(input$glucose)})
+    
+  }
+  
+  
+  
+  
+)
